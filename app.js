511ee85bded29559e6ca838973e5283ff27cc281
IsisMejia <isismejiacarvajal@gmail.com>
2025-09-21 21:40:00 -0600

    final animigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 let amigos = [];
+
+// se agrega nombre
+function agregarAmigo() {
+    let input = document.getElementById("amigo"); 
+    let nombre = input.value.trim(); 
+
+    // Validar que no esté vacío
+    if (nombre === "") {
+        alert(" Ingresa un nombre");
+        return;
+    }
+
+    // Agregar al array
+    amigos.push(nombre);
+
+    
+
+    // Mostrar la lista actualizada
+    mostrarAmigos();
+}
+
+// Función para mostrar la lista en la pantalla
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // limpiar antes de mostrar
+
+    // Recorrer el array y crear <li> para cada nombre
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
+// Función para sortear un amigo secreto
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("⚠️ La lista está vacía, agrega nombres primero.");
+        return;
+    }
+
+    // Generar índice aleatorio
+    let indice = Math.floor(Math.random() * amigos.length);
+
+    // Mostrar resultado
+    let resultado = document.getElementById("resultado");
+    resultado.textContent = "El amigo secreto es: " + amigos[indice];
+}
+
